1c2288b903ac1ab0fcb6c09f9054b2a632677bc8
kaul <ankurkaul1894@gmail.com>
2020-07-01 16:51:20 -0700

    login working with response token

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,34 +1,31 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../../models/user')
 
 router.post('/', async (req, res) => {
     const username = req.body.username
     const password = req.body.password
-    const user = await User.findOne({ username, password })
-    // const foundUser = await User.findOne({
-    //     username: username,
-    //     password: password,
-    // })
-    console.log(user)
-    res.send(user)
-    // if (!user) {
-    //     res.status(401).send('unauthorized username')
-    // } else {
-    //     if (user.password !== password) {
-    //         res.status(401).send('unauthorized password')
-    //     } else {
-    //         const token = jwt.sign(
-    //             { user: username },
-    //             process.env.TOKEN_SECRET,
-    //             {
-    //                 expiresIn: '5h',
-    //             }
-    //         )
-    //         res.status(200).json({ token })
-    //     }
-    // }
+    const user = await User.findOne({ username })
+
+    if (!user) {
+        res.status(401).send('unauthorized credential')
+    } else {
+        const matchPassword = await bcrypt.compare(password, user.password)
+        if (!matchPassword) {
+            res.status(401).send('unauthorized credentials')
+        } else {
+            const token = jwt.sign(
+                { id: user._id, username },
+                process.env.TOKEN_SECRET,
+                {
+                    expiresIn: '1h',
+                }
+            )
+            res.status(200).send({ token })
+        }
+    }
 })
 module.exports = router
